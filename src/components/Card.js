558ad1dd566b566d36b2b8d25b3b9fe3d66f4a7a
Grzegorz Paprzycki <paprzycki.grzegorz@gmail.com>
2020-04-24 10:04:52 +0200

    Improve GuessPage.css

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,27 +5,34 @@ class Card extends Component {
 
     activeStyle = {
         backgroundColor: 'yellow',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        letterSpacing: "normal",
+
     }
 
     checkedStyle = {
         backgroundColor: this.props.color
     }
 
+    gameEndedStyle = {
+        letterSpacing: "normal",
+    }
+
     answerShownStyle = {
         backgroundColor: this.props.color,
         opacity: "35%",
+        letterSpacing: "normal",
     }
 
-
     render() {
-        if (this.props.role === "/szefowie") {
+        const { role, color, id, text, active, checked, answerShown, activeCard, gameEnded } = this.props;
+        if (role === "/szefowie") {
             return (
-                <div style={{ backgroundColor: `${this.props.color}` }} id={this.props.id}>{this.props.text}</div>
+                <div style={{ backgroundColor: `${color}` }} id={id}>{text}</div>
             );
         } else {
             return (
-                <div style={this.props.active ? this.activeStyle : this.props.checked ? this.checkedStyle : this.props.answerShown ? this.answerShownStyle : null} id={this.props.id} onClick={() => this.props.activeCard(this.props.id)} >{this.props.text}</div>
+                <div style={active ? this.activeStyle : checked ? this.checkedStyle : !gameEnded ? null : answerShown ? this.answerShownStyle : this.gameEndedStyle} id={id} onClick={() => activeCard(this.props.id)} >{text}</div>
             );
         }
     }
