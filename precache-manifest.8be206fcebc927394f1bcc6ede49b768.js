ed7ef12d10fbcd7aaffba3dbb26205d16b43edf3
Grzegorz Paprzycki <paprzycki.grzegorz@gmail.com>
2020-04-24 18:48:19 +0200

    Updates

---
+++ precache-manifest.8be206fcebc927394f1bcc6ede49b768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9910fe25bfa557c003f6c0e2aca61b",
+    "url": "/Tajniacy-Gra/index.html"
+  },
+  {
+    "revision": "da5052d31aac13c4006f",
+    "url": "/Tajniacy-Gra/static/css/main.ebd15026.chunk.css"
+  },
+  {
+    "revision": "3222e0dbbd840429df26",
+    "url": "/Tajniacy-Gra/static/js/2.841f9588.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tajniacy-Gra/static/js/2.841f9588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5052d31aac13c4006f",
+    "url": "/Tajniacy-Gra/static/js/main.ba9b931c.chunk.js"
+  },
+  {
+    "revision": "7e3fc14f6f8e4773c72c",
+    "url": "/Tajniacy-Gra/static/js/runtime-main.170d2392.js"
+  }
+]);
